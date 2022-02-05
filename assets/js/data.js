8a9d2c0e69469f6a77d26a9bf2186f8dd26e97dc
kembreyfarquhar <kembreyfarquhar@gmail.com>
2022-02-05 06:19:53 -0600

    creates data js file

---
+++ assets/js/data.js
@@ -0,0 +1,13 @@
+const HEADS = {
+  src: 'assets/images/penny-heads.jpg',
+  message: 'You flipped heads!',
+}
+
+const TAILS = {
+  src: 'assets/images/penny-tails.jpg',
+  message: 'You flipped tails!',
+}
+
+const DEF_MSG = "Let's get rolling!"
+const DEF_SRC = HEADS.src
+const DEF_PERC = '0%'
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-const HEADS = {
-  src: 'assets/images/penny-heads.jpg',
-  message: 'You flipped heads!',
-}
-
-const TAILS = {
-  src: 'assets/images/penny-tails.jpg',
-  message: 'You flipped tails!',
-}
-
-const DEF_MSG = "Let's get rolling!"
-const DEF_SRC = HEADS.src
-const DEF_PERC = '0%'
-
 let totalCt = 0,
   headsCt = 0,
   tailsCt = 0
