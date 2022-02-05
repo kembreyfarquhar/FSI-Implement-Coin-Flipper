5106a6869cec183982625f537cc6ec0a1b081154
kembreyfarquhar <kembreyfarquhar@gmail.com>
2022-02-05 06:14:22 -0600

    refactor; less lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,33 @@
-const headsData = {
+const HEADS = {
   src: 'assets/images/penny-heads.jpg',
   message: 'You flipped heads!',
 }
 
-const tailsData = {
+const TAILS = {
   src: 'assets/images/penny-tails.jpg',
   message: 'You flipped tails!',
 }
 
-const defaultMessage = "Let's get rolling!"
-const defaultSrc = headsData.src
-const defaultPercentage = '0%'
+const DEF_MSG = "Let's get rolling!"
+const DEF_SRC = HEADS.src
+const DEF_PERC = '0%'
 
-let totalCount = 0
-let headsCount = 0
-let tailsCount = 0
+let totalCt = 0,
+  headsCt = 0,
+  tailsCt = 0
 
-const makeTie = elementsArray => elementsArray.forEach(element => (element.classList = 'tie'))
-const makeWin = element => (element.classList = 'winning')
-const makeLose = element => (element.classList = 'losing')
+const makeTie = elemArr => elemArr.forEach(elem => (elem.classList = 'tie'))
+const makeWin = elem => (elem.classList = 'winning')
+const makeLose = elem => (elem.classList = 'losing')
 
 document.addEventListener('DOMContentLoaded', () => {
   // ================================== DOM ELEMENTS  ================================== //
   let flipBtn = document.getElementById('flip')
   let clearBtn = document.getElementById('clear')
-  let headsCountData = document.getElementById('heads')
-  let headsPercentData = document.getElementById('heads-percent')
-  let tailsCountData = document.getElementById('tails')
-  let tailsPercentData = document.getElementById('tails-percent')
+  let headsCtData = document.getElementById('heads')
+  let headsPercData = document.getElementById('heads-percent')
+  let tailsCtData = document.getElementById('tails')
+  let tailsPercData = document.getElementById('tails-percent')
   let pennyImg = document.getElementById('penny-image')
   let message = document.getElementById('message')
 
@@ -35,44 +35,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const events = new EventEmitter()
 
   events.subscribe('FLIP', isHeads => {
-    totalCount++
-    if (isHeads) {
-      headsCount++
-      headsCountData.textContent = headsCount
-    } else {
-      tailsCount++
-      tailsCountData.textContent = tailsCount
-    }
-    pennyImg.src = isHeads ? headsData.src : tailsData.src
-    message.textContent = isHeads ? headsData.message : tailsData.message
+    totalCt++
+    if (isHeads) headsCt++, (headsCtData.textContent = headsCt)
+    else tailsCt++, (tailsCtData.textContent = tailsCt)
+    pennyImg.src = isHeads ? HEADS.src : TAILS.src
+    message.textContent = isHeads ? HEADS.message : TAILS.message
   })
 
   events.subscribe('CALCULATE_PERCENTAGES', () => {
-    const headsPercentage = Math.round((headsCount / totalCount) * 100)
-    const tailsPercentage = Math.round((tailsCount / totalCount) * 100)
-    headsPercentData.textContent = `${headsPercentage}%`
-    tailsPercentData.textContent = `${tailsPercentage}%`
-    const isTie = headsPercentage === tailsPercentage
-    const isHeadsWinning = headsPercentage > tailsPercentage
-    if (isTie) makeTie([headsPercentData, tailsPercentData])
-    else if (isHeadsWinning) {
-      makeWin(headsPercentData)
-      makeLose(tailsPercentData)
+    const headsPerc = Math.round((headsCt / totalCt) * 100)
+    const tailsPerc = Math.round((tailsCt / totalCt) * 100)
+    headsPercData.textContent = `${headsPerc}%`
+    tailsPercData.textContent = `${tailsPerc}%`
+    const isTie = headsPerc === tailsPerc
+    const headsWin = headsPerc > tailsPerc
+    if (isTie) makeTie([headsPercData, tailsPercData])
+    else if (headsWin) {
+      makeWin(headsPercData)
+      makeLose(tailsPercData)
     } else {
-      makeWin(tailsPercentData)
-      makeLose(headsPercentData)
+      makeWin(tailsPercData)
+      makeLose(headsPercData)
     }
   })
 
   events.subscribe('RESET', () => {
-    ;(totalCount = 0), (headsCount = 0), (tailsCount = 0)
-    pennyImg.src = defaultSrc
-    message.textContent = defaultMessage
-    headsCountData.textContent = 0
-    tailsCountData.textContent = 0
-    headsPercentData.textContent = 0
-    tailsPercentData.textContent = defaultPercentage
-    makeTie([headsPercentData, tailsPercentData])
+    ;(totalCt = 0), (headsCt = 0), (tailsCt = 0)
+    ;(pennyImg.src = DEF_SRC), (message.textContent = DEF_MSG)
+    ;(headsCtData.textContent = 0), (tailsCtData.textContent = 0)
+    ;(headsPercData.textContent = DEF_PERC), (tailsPercData.textContent = DEF_PERC)
+    makeTie([headsPercData, tailsPercData])
   })
 
   // ==================================  FLIP CLICK  ================================== //
