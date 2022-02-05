28231142a261d64c173534bb2cc99a5c9c5f4dcd
kembreyfarquhar <kembreyfarquhar@gmail.com>
2022-02-05 05:50:27 -0600

    implements eventemitter; uses subscribe and emit for click handlers

---
+++ assets/js/eventEmitter.js
@@ -0,0 +1,18 @@
+class EventEmitter {
+  constructor(events) {
+    this.events = events || {}
+  }
+
+  subscribe(name, cb) {
+    ;(this.events[name] || (this.events[name] = [])).push(cb)
+
+    return {
+      unsubscribe: () =>
+        this.events[name] && this.events[name].splice(this.events[name].indexOf(cb) >>> 0, 1),
+    }
+  }
+
+  emit(name, ...args) {
+    ;(this.events[name] || []).forEach(fn => fn(...args))
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,88 @@
-// TODO: Declare any global variables we need
-let totalCount = 0;
-let headsCount = 0;
-let tailsCount = 0;
+const headsData = {
+  src: 'assets/images/penny-heads.jpg',
+  message: 'You flipped heads!',
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  // This is just a sanity check to make sure your JavaScript script is getting loaded
-  // You can remove it once you see it in your browser console in the developer tools
-  console.log("Hi");
+const tailsData = {
+  src: 'assets/images/penny-tails.jpg',
+  message: 'You flipped tails!',
+}
 
-  // DOM elements
-  // BUTTONS
-  let flipBtn = document.getElementById("flip");
-  let clearBtn = document.getElementById("clear");
-  // SCOREBOARD CELLS
-  let headsCountData = document.getElementById("heads");
-  let headsPercentData = document.getElementById("heads-percent");
-  let tailsCountData = document.getElementById("tails");
-  let tailsPercentData = document.getElementById("tails-percent");
-  // IMG
-  let pennyImg = document.getElementById("penny-img");
-  // MESSAGE
-  let message = document.getElementById("message");
+const defaultMessage = "Let's get rolling!"
+const defaultSrc = headsData.src
+const defaultPercentage = '0%'
 
-  // TODO: Add event listener and handler for flip and clear buttons
+let totalCount = 0
+let headsCount = 0
+let tailsCount = 0
 
-  // Flip Button Click Handler
-  flipBtn.addEventListener("click", function () {
-    // TODO: Determine flip outcome
-    totalCount++;
-    let flippedHeads = Math.random() < 0.5;
+const makeTie = elementsArray => elementsArray.forEach(element => (element.classList = 'tie'))
+const makeWin = element => (element.classList = 'winning')
+const makeLose = element => (element.classList = 'losing')
 
-    // if (check) - what did they flip?
-    if (flippedHeads) {
-      pennyImg.src = "assets/images/penny-heads.jpg";
-      message.textContent = "You flipped heads!";
-      headsCount++;
-      headsCountData.textContent = headsCount;
+document.addEventListener('DOMContentLoaded', function () {
+  // ================================== DOM ELEMENTS  ================================== //
+  let flipBtn = document.getElementById('flip')
+  let clearBtn = document.getElementById('clear')
+  let headsCountData = document.getElementById('heads')
+  let headsPercentData = document.getElementById('heads-percent')
+  let tailsCountData = document.getElementById('tails')
+  let tailsPercentData = document.getElementById('tails-percent')
+  let pennyImg = document.getElementById('penny-image')
+  let message = document.getElementById('message')
+
+  // ==================================  EVENTS  ================================== //
+  const events = new EventEmitter()
+
+  events.subscribe('FLIP', isHeads => {
+    totalCount++
+    if (isHeads) {
+      headsCount++
+      headsCountData.textContent = headsCount
     } else {
-      pennyImg.src = "assets/images/penny-tails.jpg";
-      message.textContent = "You flipped tails!";
-      tailsCount++;
-      tailsCountData.textContent = tailsCount;
+      tailsCount++
+      tailsCountData.textContent = tailsCount
     }
+    pennyImg.src = isHeads ? headsData.src : tailsData.src
+    message.textContent = isHeads ? headsData.message : tailsData.message
+  })
 
-    let headsPercentage = Math.round((headsCount / totalCount) * 100);
-    let tailsPercentage = Math.round((tailsCount / totalCount) * 100);
-    headsPercentData.textContent = `${headsPercentage}%`;
-    tailsPercentData.textContent = `${tailsPercentage}%`;
+  events.subscribe('CALCULATE_PERCENTAGES', () => {
+    const headsPercentage = Math.round((headsCount / totalCount) * 100)
+    const tailsPercentage = Math.round((tailsCount / totalCount) * 100)
+    headsPercentData.textContent = `${headsPercentage}%`
+    tailsPercentData.textContent = `${tailsPercentage}%`
+    const isTie = headsPercentage === tailsPercentage
+    const isHeadsWinning = headsPercentage > tailsPercentage
+    if (isTie) makeTie([headsPercentData, tailsPercentData])
+    else if (isHeadsWinning) {
+      makeWin(headsPercentData)
+      makeLose(tailsPercentData)
+    } else {
+      makeWin(tailsPercentData)
+      makeLose(headsPercentData)
+    }
+  })
 
-    // TODO: Update image and status message in the DOM
+  events.subscribe('RESET', () => {
+    ;(totalCount = 0), (headsCount = 0), (tailsCount = 0)
+    pennyImg.src = defaultSrc
+    message.textContent = defaultMessage
+    headsCountData.textContent = 0
+    tailsCountData.textContent = 0
+    headsPercentData.textContent = 0
+    tailsPercentData.textContent = defaultPercentage
+    makeTie([headsPercentData, tailsPercentData])
+  })
 
-    // Update the scorboard
-    // TODO: Calculate the total number of rolls/flips
-    // Make variables to track the percentages of heads and tails
-    // TODO: Use the calculated total to calculate the percentages
-    // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-    // TODO: Update the display of each table cell
-  });
+  // ==================================  FLIP CLICK  ================================== //
+  flipBtn.addEventListener('click', function () {
+    events.emit('FLIP', Math.random() < 0.5)
+    events.emit('CALCULATE_PERCENTAGES')
+  })
 
-  // Clear Button Click Handler
-  // TODO: Reset global variables to 0
-  // TODO: Update the scoreboard (same logic as in flip button click handler)
-});
+  // ==================================  CLEAR CLICK  ================================== //
+  clearBtn.addEventListener('click', function () {
+    events.emit('RESET')
+  })
+})
